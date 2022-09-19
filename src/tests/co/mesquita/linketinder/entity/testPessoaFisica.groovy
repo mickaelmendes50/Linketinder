4c37b36bd2d974b759faf7a67d81b28ee528669c
Mesquita <mesquita@aospa.co>
2022-09-19 17:55:00 -0300

    tests: Implement tests for PessoaFisica

---
+++ src/tests/co/mesquita/linketinder/entity/testPessoaFisica.groovy
@@ -0,0 +1,59 @@
+package tests.co.mesquita.linketinder.entity
+
+import co.mesquita.linketinder.entity.Pessoa
+import co.mesquita.linketinder.entity.PessoaFisica
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.*
+
+class testPessoaFisica {
+
+    @Test
+    void testGetAge() {
+        Pessoa pf = new PessoaFisica("João", "joao@worker.com", "123.456.789-01", 23, "Goiás", "74659-156", "Legal e extrovertido")
+        if (pf.getAge().equals("23"))
+            assertTrue(true)
+        else
+            assertTrue(false)
+    }
+
+    @Test
+    void testGetEstate() {
+        Pessoa pf = new PessoaFisica("João", "joao@worker.com", "123.456.789-01", 23, "Goiás", "74659-156", "Legal e extrovertido")
+        if (pf.getEstate().equals("Goiás"))
+            assertTrue(true)
+        else
+            assertTrue(false)
+    }
+
+    @Test
+    void testGetCep() {
+        Pessoa pf = new PessoaFisica("João", "joao@worker.com", "123.456.789-01", 23, "Goiás", "74659-156", "Legal e extrovertido")
+        if (pf.getCep().equals("74659-156"))
+            assertTrue(true)
+        else
+            assertTrue(false)
+    }
+
+    @Test
+    void testGetDescription() {
+        Pessoa pf = new PessoaFisica("João", "joao@worker.com", "123.456.789-01", 23, "Goiás", "74659-156", "Legal e extrovertido")
+        if (pf.getDescription().equals("Legal e extrovertido"))
+            assertTrue(true)
+        else
+            assertTrue(false)
+    }
+
+    @Test
+    void testGetSkills () {
+        Pessoa pf = new PessoaFisica("João", "joao@worker.com", "123.456.789-01", 23, "Goiás", "74659-156", "Legal e extrovertido")
+        pf.addSkill("Java")
+        pf.addSkill("JUnit")
+
+        if (pf.getSkills().equals("Java, JUnit, "))
+            assertTrue(true)
+        else
+            assertTrue(false)
+    }
+}
